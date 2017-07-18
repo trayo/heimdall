52c0d8ddeb280642756fa7f436afa7111db69314
Travis Yoder <trayoda@gmail.com>
2017-07-18 15:45:57 -0700

    add disk encryption

--- run.sh
+++ run.sh
@@ -5,12 +5,13 @@ set -e
 DISK=""
 USERNAME="TravisYoder"
 EMAIL="tyodf@allstate.com"
+PASSWORD=""
 
 # grab device identifier and device node
 DISK=$(diskutil info $USERNAME | grep "Part of Whole" | grep -o "disk.*")
 
 # format disk
-diskutil eraseDisk HFS+ $USERNAME MBR $DISK
+diskutil eraseDisk JHFS+ $USERNAME $DISK
 
 sleep 5
 
@@ -26,4 +27,8 @@ mkdir /Volumes/$USERNAME/.ssh
 # generate private/public key on disk
 ssh-keygen -N '' -f /Volumes/$USERNAME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
 
-# ask for password to encrypt disk
+# ask for password to encrypt the disk
+read -s -p "Enter Password: " PASSWORD
+
+# encrypt the disk
+diskutil cs convert /Volumes/$USERNAME -passphrase $PASSWORD
